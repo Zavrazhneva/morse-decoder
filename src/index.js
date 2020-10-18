a1f5aff4937b3e6f20a0f4ff74abd316eccf7287
Anastasiya Zavrazhneva <zavrazhneva.a@gmail.com>
2020-10-18 10:36:04 +0300

    feat: add morse-decoder function decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decodeWordrArr = [];
+    for(let i = 0; i <= expr.length; i += 10 ) {
+        let decodeLetter = expr.slice(i, i + 10);
+        let decodeLetterArr = [];
+
+        if(decodeLetter === '**********') decodeWordrArr.push(' ')
+        for(let i = 0; i <= decodeLetter.length; i += 2 ) {
+            let decodeNewArr = decodeLetter.slice(i, i + 2);
+            decodeLetterArr.push(decodeNewArr.replace(/10/g, '.').replace(/11/g, '-').replace(/00/g, ''))
+        }
+        decodeWordrArr.push(MORSE_TABLE[decodeLetterArr.join('')])
+    }
+    return decodeWordrArr.join('')
 }
 
 module.exports = {
